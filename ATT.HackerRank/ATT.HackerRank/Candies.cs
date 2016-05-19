2a493af1e28afea8afe0405ef26baf901330187e
Lee Dale <lee.jdale@gmail.com>
2016-05-19 06:38:10 +0000

    Added HackerRank challenge Candies

---
+++ ATT.HackerRank/ATT.HackerRank/Candies.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int numberChildren = int.Parse(Console.ReadLine());
+        int[] ratings = new int[numberChildren];
+
+        for (int index = 0; index < numberChildren; index++)
+        {
+            ratings[index] = int.Parse(Console.ReadLine());
+        }
+
+        int[] candies = GiveOutCandiesUp(new int[numberChildren], ratings);
+        candies = GiveOutCandiesDown(candies, ratings);
+        Console.WriteLine(candies.Sum());
+    }
+
+    private static int[] GiveOutCandiesUp(int[] candies, int[] ratings)
+    {
+        for (int index = 0; index < ratings.Length; index++)
+        {
+            if (index == 0)
+            {
+                candies[index] = 1;
+                continue;
+            }
+
+            int myCandies = candies[index];
+            int myRating = ratings[index];
+            int beforeCandies = candies[index - 1];
+            int beforeRating = ratings[index - 1];
+            
+            if (myRating > beforeRating)
+            {
+                candies[index] = beforeCandies + 1;
+            }
+
+            if (myRating == beforeRating)
+            {
+                candies[index] = beforeCandies - 1;
+            }
+
+            if (candies[index] == 0) candies[index] = 1;
+        }
+
+        return candies;
+    }
+
+    private static int[] GiveOutCandiesDown(int[] candies, int[] ratings)
+    {
+        for (int index = ratings.Length -1; index > 0; index--)
+        {
+            int myCandies = candies[index];
+            int myRating = ratings[index];
+            int beforeCandies = candies[index - 1];
+            int beforeRating = ratings[index - 1];
+
+            if (myRating > beforeRating)
+            {
+                candies[index] = beforeCandies +1;
+            }
+
+            if (candies[index] == 0) candies[index] = 1;
+        }
+
+        return candies;
+    }
+}
