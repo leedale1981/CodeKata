7b71fef4270fb248d16da8e5deaff88373f860ac
Lee Dale <lee.jdale@gmail.com>
2016-05-20 11:21:54 +0100

    Added HackerRank challenge

---
+++ ATT.HackerRank/ATT.HackerRank/Operators.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATT.HackerRank
+{
+    class Operators
+    {
+        static void Main(String[] args)
+        {
+            double mealCost = Convert.ToDouble(Console.ReadLine());
+            int tipPercent = Convert.ToInt32(Console.ReadLine());
+            int taxPercent = Convert.ToInt32(Console.ReadLine());
+
+            int totalCost = CalculateMealCost(mealCost, tipPercent, taxPercent);
+            Console.WriteLine("The total meal cost is " + totalCost + " dollars.");
+        }
+
+        private static int CalculateMealCost(double mealCost, int tipPercent, int taxPercent)
+        {
+            double tip = mealCost * tipPercent / 100;
+            double tax = mealCost * taxPercent / 100;
+
+            return  Convert.ToInt32(Math.Round(mealCost + tip + tax, 1));
+        }
+    }
+}
