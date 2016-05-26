eb6ba9b5a3490b1c6fec780f6201799a76b25262
Lee Dale <lee.jdale@gmail.com>
2016-05-26 16:42:54 +0000

    Updated from VM

---
+++ ATT.HackerRank/ATT.HackerRank/Arrays2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Arrays2
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+        int[] reversed = ForReverse(arr);
+
+        for (int index = 0; index < reversed.Length; index++)
+        {
+            Console.Write(reversed[index] + " ");
+        }
+    }
+
+    private static int[] ForReverse(int[] input)
+    { 
+        int[] output = new int[input.Length];
+        int count = 0;
+
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            output[i] = input[count];
+            count++;
+        }
+
+        return output;
+    }
+}
---
+++ ATT.HackerRank/ATT.HackerRank/LetsReview.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class LetsReview
+{
+    static void Main(String[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        string[] i =  new string[N];
+
+        for (int index = 0; index < N; index++)
+        {
+            i[index] = Console.ReadLine();
+        }
+
+        PrintStrings(i, N);
+    }
+
+    private static void PrintStrings(string[] i, int N)
+    {
+        for (int sindex = 0; sindex < N; sindex++)
+        {
+            string even = string.Empty;
+            string odd = string.Empty;
+            char[] chars = i[sindex].ToCharArray();
+
+            for (int cindex = 0; cindex < chars.Length; cindex++)
+            {
+                if (cindex % 2 == 0)
+                {
+                    even += chars[cindex];
+                }
+                else
+                {
+                    odd += chars[cindex];
+                }
+            }
+
+            Console.WriteLine(even + " " + odd);
+        }
+    }
+}
