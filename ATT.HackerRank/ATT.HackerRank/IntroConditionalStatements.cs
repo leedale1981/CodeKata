d568f0629aea5bf0eb5fe8a83663bf8dce9fc3de
Lee Dale <lee.jdale@gmail.com>
2016-06-07 07:30:34 +0100

    Hacker Rank update

---
+++ ATT.HackerRank/ATT.HackerRank/IntroConditionalStatements.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class ConditionalStatements
+{
+    static void Main(String[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        switch (N % 2)
+        {
+            case 0:
+                if (N >= 2 && N <= 5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+
+                if (N >= 6 && N <= 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+
+                if (N > 20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+
+                break;
+
+            default:
+                Console.WriteLine("Weird");
+                break;
+
+        }
+    }
+}
